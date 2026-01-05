3a722ed82ea0d9cbca06e5dc6e85870592329927
Huseyin DOL <huseyindol@gmail.com>
2026-01-05 23:01:11 +0300

    feat: Add production and local server URLs to OpenAPI configuration.

--- src/main/java/com/cms/config/OpenApiConfig.java
+++ src/main/java/com/cms/config/OpenApiConfig.java
@@ -11,25 +11,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Elly CMS API")
-                        .version("1.0")
-                        .description("API documentation for Elly CMS"))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .description(
-                                                "JWT token authentication. Login endpoint'inden token alıp buraya ekleyin.")));
-    }
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Elly CMS API")
+                                                .version("1.0")
+                                                .description("API documentation for Elly CMS"))
+                                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                                                .url("https://api.huseyindol.site")
+                                                .description("Production (HTTPS)"))
+                                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                                                .url("http://localhost:8080")
+                                                .description("Local Development"))
+                                .addSecurityItem(new SecurityRequirement()
+                                                .addList(securitySchemeName))
+                                .components(new Components()
+                                                .addSecuritySchemes(securitySchemeName,
+                                                                new SecurityScheme()
+                                                                                .name(securitySchemeName)
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")
+                                                                                .description(
+                                                                                                "JWT token authentication. Login endpoint'inden token alıp buraya ekleyin.")));
+        }
 }
